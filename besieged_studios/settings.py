2578f218c37dec61ac3cea7b27915b9095aaf566
Mitchell Duke <119351506+MitchDuke@users.noreply.github.com>
2024-11-17 13:36:03 +0000

    File Structure
    Created Basic file structure and began the readme file

--- besieged_studios/settings.py
+++ besieged_studios/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
-    #Besieged Studios Apps
+
+    # Besieged Studios Apps
     'gallery',
     'commissions',
     'accounts',
