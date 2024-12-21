35bde72b9ca8dd696ccf881f4de1f0dc9b9300a4
Mitchell Duke <119351506+MitchDuke@users.noreply.github.com>
2024-12-21 13:12:17 +0000

    Basket view
    Updated basket view to display recently added items

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'basket'
 
 urlpatterns = [
+    path('', views.basket_view, name='basket_view'),
     path('add/<int:pk>/', views.add_to_basket, name='add'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from gallery.models import Project
+
+
+def basket_view(request):
+    basket = request.session.get("basket", {})
+    basket_items = []
+    for pk, quantity in basket.items():
+        try:
+            project = Project.objects.get(pk=pk)
+            basket_items.append({
+                "project": project,
+                "quantity": quantity,
+                'total_price': project.price * quantity
+            })
+        except Project.DoesNotExist:
+            continue
+    return render(request, "basket/basket.html", {"basket": basket, "basket_items": basket_items})
 
 
 def add_to_basket(request, pk):
-    return HttpResponse(f"Product {pk} added to basket (placeholder).")
+    basket = request.session.get("basket", {})
+    if pk in basket:
+        basket[pk] += 1
+    else:
+        basket[pk] = 1
+    request.session["basket"] = basket
+    messages.success(request, "Item added to basket.")
+    return redirect("basket:basket_view")
